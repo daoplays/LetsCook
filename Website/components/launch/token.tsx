74bf6fb5002c8156f80c36d6020f5cc5ab4f4ece
franzegos <71435340+franzegos@users.noreply.github.com>
2024-04-03 16:03:58 +0800

    fixed token grind progress

--- Website/components/launch/token.tsx
+++ Website/components/launch/token.tsx
@@ -109,81 +109,83 @@ const TokenPage = ({ setScreen }: TokenPageProps) => {
     // Calculate the total sum of all percentages
     const totalPercentage = getTotalPercentage(distribution);
 
-    function setLaunchData(e) {
+    let grindToast;
+
+    const est_time = tokenStart.length === 3 ? "~5 mins" : tokenStart.length === 2 ? "5s" : "1s";
+
+    async function setLaunchData(e) {
         e.preventDefault();
 
         if (totalPercentage !== 100) {
             toast.error("The total percentage must add up to 100%.");
-            return;
+            return false;
         }
 
         if (newLaunchData.current.icon_file === null) {
             toast.error("Please select an icon image.");
-            return;
+            return false;
         }
 
         if (parseFloat(ticketPrice) < 0.00001) {
             toast.error("Minimum ticket price is 0.00001 SOL");
-            return;
+            return false;
         }
 
         if (symbol.length > 10) {
             toast.error("Maximum symbol length is 10 characters");
-            return;
+            return false;
         }
 
         if (name.length > 25) {
             toast.error("Maximum name length is 25 characters");
-            return;
+            return false;
         }
 
         if (distribution[Distribution.LP] === 0) {
             toast.error("Liquidity pool allocation must be greater than zero");
-            return;
+            return false;
         }
 
         if (distribution[Distribution.Raffle] === 0) {
             toast.error("Raffle allocation must be greater than zero");
-            return;
+            return false;
         }
 
         if (Math.pow(10, parseInt(decimal)) * parseInt(totalSupply) * (distribution[Distribution.Raffle] / 100) < parseInt(mints)) {
             toast.error("Not enough tokens to support the raffle");
-            return;
+            return false;
         }
 
         if (parseInt(totalSupply) < 10) {
             toast.error("Total supply of tokens must be over 10");
-            return;
+            return false;
         }
 
         newLaunchData.current.token_keypair = Keypair.generate();
 
         if (tokenStart !== "") {
-            let est_time = "1s";
-            if (tokenStart.length == 2) est_time = "5s";
-            if (tokenStart.length === 3) est_time = "5min";
-
-            const grindToast = toast.loading("Performing token prefix grind.. Est. time:  " + est_time);
             let attempts = 0;
 
-            while (newLaunchData.current.token_keypair.publicKey.toString().substring(0, tokenStart.length) !== tokenStart) {
-                attempts += 1;
+            do {
+                attempts++;
                 let seed_buffer = [];
+
                 for (let i = 0; i < 32; i++) {
                     seed_buffer.push(Math.floor(Math.random() * 255));
                 }
+
                 let seed = new Uint8Array(seed_buffer);
 
                 newLaunchData.current.token_keypair = Keypair.fromSeed(seed);
+
                 if (attempts % 10000 === 0) {
                     console.log("attempts: " + attempts);
-                    toast.update(grindToast, {
-                        render: "Grind Attempts: " + attempts.toString(),
-                        type: "info",
-                    });
+                    // toast.update(grindToast, {
+                    //     render: "Grind Attempts: " + attempts.toString(),
+                    //     type: "info",
+                    // });
                 }
-            }
+            } while (!newLaunchData.current.token_keypair.publicKey.toString().startsWith(tokenStart));
 
             //console.log("Took ", attempts, "to get pubkey", newLaunchData.current.token_keypair.publicKey.toString());
             toast.update(grindToast, {
@@ -217,7 +219,17 @@ const TokenPage = ({ setScreen }: TokenPageProps) => {
             newLaunchData.current.transfer_hook_program = new PublicKey(transferHookID);
         }
 
-        setScreen("details");
+        return true;
+    }
+
+    async function nextPage(e) {
+        grindToast = toast.loading(
+            "Please wait while we perform the token prefix grind. Do not exit the page. Estimated time: " + est_time,
+        );
+
+        await new Promise((resolve) => setTimeout(resolve, 500));
+
+        if (await setLaunchData(e)) setScreen("details");
     }
 
     const Browse = () => (
@@ -248,7 +260,7 @@ const TokenPage = ({ setScreen }: TokenPageProps) => {
                 <Text align="start" className="font-face-kg" color={"white"} fontSize="x-large">
                     Token Information:
                 </Text>
-                <form onSubmit={setLaunchData} style={{ width: lg ? "100%" : "1200px" }}>
+                <form style={{ width: lg ? "100%" : "1200px" }}>
                     <VStack px={lg ? 4 : 12} spacing={25} mt={4}>
                         <HStack w="100%" spacing={lg ? 10 : 12} style={{ flexDirection: lg ? "column" : "row" }}>
                             {displayImg ? (
@@ -911,7 +923,13 @@ const TokenPage = ({ setScreen }: TokenPageProps) => {
                             <button type="button" className={`${styles.nextBtn} font-face-kg `} onClick={() => router.push("/dashboard")}>
                                 Cancel
                             </button>
-                            <button type="submit" className={`${styles.nextBtn} font-face-kg `}>
+                            <button
+                                type="button"
+                                className={`${styles.nextBtn} font-face-kg `}
+                                onClick={(e) => {
+                                    nextPage(e);
+                                }}
+                            >
                                 NEXT (1/3)
                             </button>
                         </HStack>
