d02bae3c54b972fa33ee55ab9319f2411214df57
Lindley Lentati <daoplays@outlook.com>
2024-04-27 14:18:04 +0100

    remove imports

--- Website/hooks/collections/useMintNFT.tsx
+++ Website/hooks/collections/useMintNFT.tsx
@@ -25,10 +25,6 @@ import {
     Keypair,
     AccountMeta,
 } from "@solana/web3.js";
-import {deserializeAssetV1} from "@metaplex-foundation/mpl-core";
-import type { RpcAccount, PublicKey as umiKey } from '@metaplex-foundation/umi';
-import { createUmi } from '@metaplex-foundation/umi-bundle-defaults';
-import { publicKey } from '@metaplex-foundation/umi';
 import { TOKEN_2022_PROGRAM_ID, ASSOCIATED_TOKEN_PROGRAM_ID, getAssociatedTokenAddress } from "@solana/spl-token";
 import { useWallet } from "@solana/wallet-adapter-react";
 import { PROGRAM, Config, SYSTEM_KEY, SOL_ACCOUNT_SEED, CollectionKeys, METAPLEX_META, CORE } from "../../components/Solana/constants";
