15ccb01c84d0985f08b0e7d24f523899b3c640b7
Lindley Lentati <daoplays@outlook.com>
2024-05-25 15:55:06 +0100

    update to secure rpc

--- Website/components/Solana/constants.tsx
+++ Website/components/Solana/constants.tsx
@@ -23,7 +23,7 @@ const MainNetConfig: NetworkConfig = {
     PYTH_SOL: new PublicKey("H6ARHf6YXhGYeQfUzQNGk6rDNnLBQKrenN712K4AQJEG"),
     FEES_KEY: new PublicKey("HtszJ5ntXnwUFc2anMzp5RgaPxtvTFojL2qb5kcFEytA"),
     RAYDIUM_FEES: new PublicKey("7YttLkHDoNj9wyDur5pM1ejNaAvT9X4eqaYcHQqtj2G5"),
-    RPC_NODE: "https://mainnet.helius-rpc.com/?api-key=8c0a541e-cdf4-4c1e-8bf9-de66a1962d6f",
+    RPC_NODE: "https://kimmie-wuj3pm-fast-mainnet.helius-rpc.com",
     WSS_NODE: "wss://mainnet.helius-rpc.com/?api-key=8c0a541e-cdf4-4c1e-8bf9-de66a1962d6f",
     IRYS_URL: "https://node2.irys.xyz",
     IRYS_WALLET: "DHyDV2ZjN3rB6qNGXS48dP5onfbZd3fAEz6C5HJwSqRD",
