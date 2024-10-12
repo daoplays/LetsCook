9e5e2b8e1b6e5ae63053e171c7aef34d5cfb21a9
Lindley Lentati <daoplays@outlook.com>
2024-10-12 12:21:53 +0100

    add to cllectionData;

--- Website/hooks/data/useCollection.tsx
+++ Website/hooks/data/useCollection.tsx
@@ -1,6 +1,9 @@
 import { useEffect, useState, useCallback, useRef } from "react";
 import useAppRoot from "../../context/useAppRoot";
 import { CollectionData, CollectionPluginData, getCollectionPlugins } from "../../components/collection/collectionState";
+import { CollectionKeys } from "../../components/Solana/constants";
+import {getTransferFeeConfig, calculateFee } from "@solana/spl-token";
+import { MintData, bignum_to_num } from "../../components/Solana/state";
 
 interface useCollectionProps {
     pageName: string | null;
@@ -11,10 +14,13 @@ const useCollection = (props: useCollectionProps | null) => {
     // State to store the token balance and any error messages
     const [collection, setCollection] = useState<CollectionData | null>(null);
     const [collectionPlugins, setCollectionPlugins] = useState<CollectionPluginData | null>(null);
+    const [tokenMint, setTokenMint] = useState<MintData | null>(null);
+    const [whitelistMint, setWhitelistMint] = useState<MintData | null>(null);
+    const [outAmount, setOutAmount] = useState<number | null>(null);
     const [error, setError] = useState<string | null>(null);
 
     // Get the collectionList from the app's root context
-    const { collectionList } = useAppRoot();
+    const { collectionList, mintData } = useAppRoot();
 
     const pageName = props?.pageName || null;
 
@@ -26,32 +32,67 @@ const useCollection = (props: useCollectionProps | null) => {
             return;
         }
 
-        if (!collectionList) {
+        if (!collectionList || !mintData) {
             setCollection(null);
-            setError("Collection list is not available");
+            setError("Data is not available");
             return;
         }
 
         const data = collectionList.get(pageName.toString());
 
-        if (!data) {
+        // get the mints
+        let token_mint = mintData.get(data.keys[CollectionKeys.MintAddress].toString());
+
+
+        if (!data || !token_mint) {
             setCollection(null);
-            setError(`Collection for ${pageName} not found`);
+            setError(`Collection or Mint for ${pageName} not found`);
             return;
         }
 
         setCollection(data);
-        setCollectionPlugins(getCollectionPlugins(data));
 
+        let plugins : CollectionPluginData = getCollectionPlugins(data);
+        setCollectionPlugins(plugins);
+
+        setTokenMint(token_mint);
+
+        let whitelist_mint = null;
+        if (plugins.whitelistKey) {
+            whitelist_mint = mintData.get(plugins.whitelistKey.toString());
+            setWhitelistMint(whitelist_mint);
+        }
+
+        // if this isn't mint only we need to calculate the swap back amount
+        if (!plugins.mintOnly) {
+            let transfer_fee_config = getTransferFeeConfig(token_mint.mint);
+            let input_fee =
+                transfer_fee_config === null ? 0 : Number(calculateFee(transfer_fee_config.newerTransferFee, BigInt(data.swap_price)));
+            let swap_price = bignum_to_num(data.swap_price);
+
+            let input_amount = swap_price - input_fee;
+
+            let swap_fee = Math.floor((input_amount * data.swap_fee) / 100 / 100);
+
+            let output = input_amount - swap_fee;
+
+            let output_fee = transfer_fee_config === null ? 0 : Number(calculateFee(transfer_fee_config.newerTransferFee, BigInt(output)));
+
+            let final_output = output - output_fee;
+
+            //console.log("actual input amount was",  input_fee, input_amount,  "fee",  swap_fee,  "output", output, "output fee", output_fee, "final output", final_output);
+            let out_amount = final_output / Math.pow(10, data.token_decimals);
+            setOutAmount(out_amount);
+        }
         setError(null);
-    }, [collectionList, pageName]);
+    }, [collectionList, mintData, pageName]);
 
     useEffect(() => {
         fetchCollection();
     }, [fetchCollection]);
 
     // Return the current token balance and any error message
-    return { collection, collectionPlugins, error };
+    return { collection, collectionPlugins, tokenMint, whitelistMint,outAmount, error };
 };
 
 export default useCollection;
