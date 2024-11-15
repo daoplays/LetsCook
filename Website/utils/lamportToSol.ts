7b0ebf1731f6693c688e24a533ececfa0e6f1efe
Dao Plays <daoplays@outlook.com>
2024-11-15 11:48:46 +0000

    fix rounding

--- Website/utils/lamportToSol.ts
+++ Website/utils/lamportToSol.ts
@@ -6,9 +6,11 @@ export function lamportsToSol(lamports: string | number): string {
         if (isNaN(numericLamports)) {
             return "N/A";
         }
-        return (numericLamports / LAMPORTS_PER_SOL).toFixed(2);
+        const sol = numericLamports / LAMPORTS_PER_SOL;
+        return parseFloat(sol.toFixed(4)).toString();
     } else if (typeof lamports === "number") {
-        return (lamports / LAMPORTS_PER_SOL).toFixed(2);
+        const sol = lamports / LAMPORTS_PER_SOL;
+        return parseFloat(sol.toFixed(4)).toString();
     } else {
         return "N/A";
     }
